a4a18cf82e5cc89fb9183c37c2b0f5894663f3f6
RodrigoPerea <chri65mb@stud.kea.dk>
2022-03-14 13:47:04 +0100

    påbegyndelse currentRoom

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,29 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-import java.awt.*;
-
 public class Main {
 
-    public static void main(String[] args) {
+  public static int main(String[] args) {
 
-        Menu run = new Menu();
+    Menu run = new Menu();
 
-        run.mainMenu();
+    run.mainMenu();
+    // opretter rummene, me dhvilken værdi det er mulgit at flytte.
+    Room room1 = new Room(null, 2, 3, null);
+    Room room2 = new Room(null, 2, 3, 4);
+    Room room3 = new Room(null, null, 3, 4);
+    Room room4 = new Room(1, 2, 3, null);
+    Room room5 = new Room(1, 2, 3, 4);
+    Room room6 = new Room(1, null, 3, 4);
+    Room room7 = new Room(1, 2, null, null);
+    Room room8 = new Room(1, 2, null, 4);
+    Room room9 = new Room(1, null, null, 4);
+    int player = 1;
+    player = currentRoom(player);
 
+  }
 
-	// write your code here
-int  player;
-
-        /*
-        // currentRoom  player
-
-/*
-        public String currentRoom (){
-            if (player.equals ){
-                System.out.println("...you cannot go that way");
-            } else if (player == Room);
-
-        }
-*/
-        // opretter rummene, me dhvilken værdi det er mulgit at flytte.
-        Room room1 = new Room (null, 2, 3 , null);
-        Room  room2 = new Room (null, 2,3, 4);
-        Room room3 = new Room(null, null, 3, 4);
-        Room room4 = new Room(1, 2, 3, null);
-        Room room5 = new Room(1, 2, 3, 4);
-        Room room6 = new Room(1, null, 3, 4);
-        Room room7 = new Room(1, 2, null, null);
-        Room room8 = new Room(1, 2, null, 4);
-        Room room9 = new Room(1, null, null, 4);
-
+      private static int currentRoom ( int player){
+        return player;
+      }
     }
-}
+
