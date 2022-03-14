ef5c4261dcf35132955f9108a3fe27cd095da523
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-03-14 19:10:39 +0100

    We can move around now!

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -56,24 +56,24 @@ public class Adventure {
         System.out.println(currentRoom.getDescription());
 
       } else if (input.equalsIgnoreCase("go north")) {
-        Room dungeonToTheNorth = currentRoom.getNorth();
+        Room dungeonNorth = currentRoom.getNorth();
         String direction = "north";
-        checkForNull(dungeonToTheNorth, direction);
+        checkForNull(dungeonNorth, direction);
 
       } else if (input.equalsIgnoreCase("go south")) {
-        Room dungeonToTheSouth = currentRoom.getSouth();
+        Room dungeonSouth = currentRoom.getSouth();
         String direction = "south";
-        checkForNull(dungeonToTheSouth, direction);
+        checkForNull(dungeonSouth, direction);
 
       } else if (input.equalsIgnoreCase("Go east")) {
-        Room dungeonToTheEast = currentRoom.getEast();
+        Room dungeonEast = currentRoom.getEast();
         String direction = "east";
-        checkForNull(dungeonToTheEast, direction);
+        checkForNull(dungeonEast, direction);
 
       } else if (input.equalsIgnoreCase("Go west")) {
-        Room dungeonToTheWest = currentRoom.getWest();
+        Room dungeonWest = currentRoom.getWest();
         String direction = "west";
-        checkForNull(dungeonToTheWest, direction);
+        checkForNull(dungeonWest, direction);
 
       } else if (input.equals("help")) {
         System.out.println("You use the commands " + "go north\n" +
@@ -122,37 +122,50 @@ public class Adventure {
     Room dungeon8 = new Room("Dungeon 8", "description");
     Room dungeon9 = new Room("Dungeon 9", "description");
 
+    //room 1
     dungeon1.setEast(dungeon2);
     dungeon1.setSouth(dungeon4);
-
+    //Room 2
     dungeon2.setWest(dungeon1);
     dungeon2.setEast(dungeon3);
-
+    //Room 3
     dungeon3.setWest(dungeon2);
     dungeon3.setSouth(dungeon6);
-
+    //Room 4
     dungeon4.setNorth(dungeon1);
     dungeon4.setSouth(dungeon7);
-
+    //Room 5
     dungeon5.setSouth(dungeon8);
-
+    //Room 6
     dungeon6.setNorth(dungeon3);
     dungeon6.setSouth(dungeon9);
-
+    //Room 7
     dungeon7.setNorth(dungeon4);
     dungeon7.setEast(dungeon8);
-
+    //Room 8
     dungeon8.setWest(dungeon7);
     dungeon8.setNorth(dungeon5);
     dungeon8.setEast(dungeon9);
-
+    //Room 9
     dungeon9.setWest(dungeon8);
     dungeon9.setNorth(dungeon6);
 
     currentRoom = dungeon1;
+
+    /*if (currentRoom.getNorth() != null) { //tjekker for om den er =null
+      currentRoom = currentRoom.getNorth();//skiftet plads
+    } else if (currentRoom.getSouth() != null) { //tjekker for om den er =null
+      currentRoom = currentRoom.getSouth();//skiftet plads
+    }  else if (currentRoom.getEast() != null) { //tjekker for om den er =null
+      currentRoom = currentRoom.getEast();//skiftet plads
+    } else if (currentRoom.getWest() != null) { //tjekker for om den er =null
+      currentRoom = currentRoom.getWest();//skiftet plads
+    } else {
+      System.out.println("wrong way");
+    }
   }
-
-
+*/
+  }
   public static void main(String[] args) {
 
     Adventure adventure = new Adventure();
