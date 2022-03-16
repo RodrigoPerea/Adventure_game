d43e9b47659307d632f2bc612c3e249be8c0be58
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-03-16 13:14:51 +0100

    Class flyttet

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -35,11 +35,16 @@ public class Adventure {
 
   public void mainMenu() {
 
+    Map map = new Map();
+
+    map.makeDungeons();
+
+    currentRoom = map.getStarterRoom();
+
     Scanner sc = new Scanner(System.in);
 
     welcomeMessage();
 
-    makeDungeons();
 
     String input;
 
@@ -136,72 +141,6 @@ public class Adventure {
       System.out.println("Going " + direction);
       currentRoom = dungeon;
     }
-
-  }
-
-
-
-  public void makeDungeons() {
-
-
-    Room dungeon1 = new Room(magenta+"You are in the parking basement!",fReset+ "Here in the basement, there is a few zombies walk around\n" +
-            "shhh be quiet or they will see you!");
-
-    Room dungeon2 = new Room(magenta+"You found some stairs, and are on the first floor!",fReset+ "You found the stairs, and went upstairs.\n" +
-            "You are now on the first floor, and it smells like death\n" +
-            "You look over towards the dinning area and see a something laying over a dead corpse\n" +
-            red + "WATCH OUT IT'S A ZOMBIE " + fReset);
-
-    Room dungeon3 = new Room(magenta+"You are in one of the restaurants in the mall!",fReset+ "You are now hiding coss one of the zombie followed you, but luckily have lost track of you.\n" +
-            "You look for a knife but all you can find is a frying pan.\nMove on quick.....but quite!!");
-
-    Room dungeon4 = new Room(magenta+"You found some stairs, and are now on the second floor in the hallway.",fReset+ "you are too exposed here in the hallway. the corridor is dark, and the light is broken, and hangning from the seal...blinking.\n" + //ændret, da man jo også kan gå ned igen cr
-            "You hear some weird noises coming from somewhere in the halls.\n" +
-            "You spot a giant zombie wandering the hallway. Better move on quick, or do something else.");
-
-    Room dungeon5 = new Room(magenta+"You are now on the roof, close to the helicopter-patch.",fReset+ "You have finally arrived at the helicopter area. Try quick to block the door to the stairs, so you can wait for the helicopter.\n" +
-            "The zombies are trying to get through the door. What do you do now?");
-
-    Room dungeon6 = new Room(magenta+"You are now at an unknown destroyed staircase.",fReset+ "The staircase...? it looks like its destroyed by the attacks.\n" +
-            "You look around and spot a crate. Maybe you should look there to find something you can use to move on before someone hear you!");
-
-    Room dungeon7 = new Room(magenta+"You are in a TAAS shop.",fReset+ "You spot the american football section, maybe you should look for some football equipment for protection?");
-
-    Room dungeon8 = new Room(magenta+"You at a dead end!",fReset+ "A zombie, as huge as a tank, is blocking some stairs, that leads up to the roof. Find another way, or somthing to deal with the zombi.");
-
-    Room dungeon9 = new Room(magenta+"You are in the OUTDOOR911.",fReset+ "You found the hunting section. Maybe you should look for a rifle with a few shells?\n" +
-            "You can maybe use it against the giant zombie, that's blocking the door");
-
-    //room 1
-    dungeon1.setEast(dungeon2);
-    dungeon1.setSouth(dungeon4);
-    //Room 2
-    dungeon2.setWest(dungeon1);
-    dungeon2.setEast(dungeon3);
-    //Room 3
-    dungeon3.setWest(dungeon2);
-    dungeon3.setSouth(dungeon6);
-    //Room 4
-    dungeon4.setNorth(dungeon1);
-    dungeon4.setSouth(dungeon7);
-    //Room 5
-    dungeon5.setSouth(dungeon8);
-    //Room 6
-    dungeon6.setNorth(dungeon3);
-  //dungeon6.setSouth(dungeon9); // lukket adgang. skal åbnes/eleminere zombie
-    //Room 7
-    dungeon7.setNorth(dungeon4);
-    dungeon7.setEast(dungeon8);
-    //Room 8
-    dungeon8.setWest(dungeon7);
-    dungeon8.setNorth(dungeon5);
-    dungeon8.setEast(dungeon9);
-    //Room 9
-    dungeon9.setWest(dungeon8);
-    //dungeon9.setNorth(dungeon6); // lukket adgang. skal åbnes/eleminere zombie
-
-    currentRoom = dungeon1;
-
 
   }
 
